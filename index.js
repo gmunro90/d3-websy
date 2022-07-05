94d0536ffd935f9375c0f230aa5bc0d2b8b6ded4
Greg Munro <gregmunro@Websy-Greg.local>
2022-07-05 15:53:04 +0200

    bar chart

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-// javascript
-var dataset = [1, 2, 3, 4, 5];
+const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-d3.select('body')
-    .selectAll('p') 
-    .data(dataset)
-    .enter()
-    .append('p') // appends paragraph for each data element
-    // .text('D3 testing');
-    .text(function(d) { return d; }); //uses data as its parameter and returns it
+const svgWidth = 500, svgHeight = 300, barPadding = 5;
+const barWidth = (svgWidth / dataset.length);
+ 
+ 
+const svg = d3.select('svg')
+     .attr("width", svgWidth)
+     .attr("height", svgHeight);
+     
+const barChart = svg.selectAll("rect")
+     .data(dataset)
+     .enter()
+     .append("rect")
+     .attr("y", function(d) {
+          return svgHeight - d 
+     })
+     .attr("height", function(d) { 
+         return d; 
+     })
+     .attr("width", barWidth - barPadding)
+     .attr("transform", function (d, i) {
+         const translate = [barWidth * i, 0]; 
+         return "translate("+ translate +")";
+     });
+ 
+const text = svg.selectAll('text')
+     .data(dataset)
+     .enter()
+     .append('text')
+     .text(function(d) {
+      return d;
+     })
+     .attr('y', function (d, i) {
+      return svgHeight - d - 2
+     })
+     .attr('x', function (d, i) {
+      return barWidth * i 
+     })
+     .attr('fill', "#FFFFFF")
