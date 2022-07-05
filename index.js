deacbf828402eb7be8d66827f5a001f6a3af6471
Greg Munro <gregmunro@Websy-Greg.local>
2022-07-05 17:02:33 +0200

    basic academind chart

--- index.js
+++ index.js
@@ -1,40 +1,29 @@
-const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+const dataset = [
+  {
+    id: 'd1',
+    value: 10,
+    region: 'USA'
+  },
+  {
+    id: 'd2',
+    value: 19,
+    region: 'Germany'
+  },
+  {
+    id: 'd3',
+    value: 12,
+    region: 'Spain'
+  },
+]
 
-const svgWidth = 500, svgHeight = 300, barPadding = 5;
-const barWidth = (svgWidth / dataset.length);
- 
- 
-const svg = d3.select('svg')
-     .attr("width", svgWidth)
-     .attr("height", svgHeight);
-     
-const barChart = svg.selectAll("rect")
-     .data(dataset)
-     .enter()
-     .append("rect")
-     .attr("y", function(d) {
-          return svgHeight - d 
-     })
-     .attr("height", function(d) { 
-         return d; 
-     })
-     .attr("width", barWidth - barPadding)
-     .attr("transform", function (d, i) {
-         const translate = [barWidth * i, 0]; 
-         return "translate("+ translate +")";
-     });
- 
-const text = svg.selectAll('text')
-     .data(dataset)
-     .enter()
-     .append('text')
-     .text(function(d) {
-      return d;
-     })
-     .attr('y', function (d, i) {
-      return svgHeight - d - 2
-     })
-     .attr('x', function (d, i) {
-      return barWidth * i 
-     })
-     .attr('fill', "#FFFFFF")
+const container = d3.select('div')
+  .classed('container', true)
+  .style('border', '1px solid black')
+
+  container.selectAll('bar')
+    .data(dataset)
+    .enter()
+    .append('div')
+    .classed('bar', true)
+    .style('width', '50px')
+    .style('height', data => (data.value * 10) + 'px')
